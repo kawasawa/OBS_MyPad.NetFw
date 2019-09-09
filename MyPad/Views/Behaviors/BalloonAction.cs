27d194eb4fe7fe538f98c4daf41dcbd4e5777ead
粟澤和紀 <k.awasawa@outlook.com>
2019-09-09 21:32:24 +0900

    [update] 軽微な変更

--- MyPad/Views/Behaviors/BalloonAction.cs
+++ MyPad/Views/Behaviors/BalloonAction.cs
@@ -1,19 +1,15 @@
 ﻿using Hardcodet.Wpf.TaskbarNotification;
 using MyLib.Wpf.Interactions;
-using Prism.Interactivity.InteractionRequest;
+using System.Windows;
 
 namespace MyPad.Views.Behaviors
 {
     public class BalloonAction : MessageAction
     {
-        protected override void Invoke(InteractionRequestedEventArgs args)
+        protected override void Invoke(MessageNotification context, MessageKind kind, string title, string message, string detail, Window owner)
         {
-            if (!(args.Context is MessageNotification context) || !(this.AssociatedObject is TaskbarIcon taskbarIcon))
+            if (!(this.AssociatedObject is TaskbarIcon taskbarIcon))
                 return;
-
-            var kind = this.Kind.HasValue ? this.Kind : context.Kind;
-            var message = string.IsNullOrEmpty(this.Message) == false ? this.Message : context.Content?.ToString() ?? string.Empty;
-            var detail = string.IsNullOrEmpty(this.Detail) == false ? this.Detail : context.Detail?.ToString() ?? string.Empty;
 
             BalloonIcon image;
             switch (kind)
@@ -31,7 +27,6 @@ namespace MyPad.Views.Behaviors
             }
 
             taskbarIcon.ShowBalloonTip(message, detail, image);
-            args.Callback();
         }
     }
 }
